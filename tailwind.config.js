995a4a06ed49bb9fe8a08465a844bf41490b7cc3
jagath <jagathbotheju@gmail.com>
2023-01-02 15:36:06 +0300

    app complete

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        roboto: ["Roboto"],
+        hachi: ["Hachi Maru Pop"],
+      },
+    },
+  },
+  plugins: [],
+};
